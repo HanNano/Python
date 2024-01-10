5af4f5637f038cf0a800749d2a9f4e3a3153c18f
HanNano <linnaymin.min09@gmail.com>
2024-01-11 00:42:06 +0630

    pytest code

---
+++ variables.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+#name = Nokali
+
+name = input("What is your name? ")
+
+print("Hello,Welcome " + name +"\nFrom my Coffee Shop.")
+
+menu = "Black Coffee, Espresso, Latte, Cappucino"
+
+print(name +  " Please,choice here\n" + menu +"\nToday is free for you.Thanks You.Coffee Time!!!! Blah Blah")
+
+order = input()
+
+print("Good choice " + name + ", we'll have that " + order + " ready for you.Blah Blah !!!!!!!!")
